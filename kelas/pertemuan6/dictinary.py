f196c3ed4e105eba2f39b99d6f1a326bba9ac164
FrogyAI <erfandimarselianus2@gmail.com>
2025-10-21 20:59:09 +0800

    add file pertemuan

---
+++ kelas/pertemuan6/dictinary.py
@@ -0,0 +1,56 @@
+
+# Daftar_buku = {
+#     "Buku1" : "Bumi Manusia",
+#     "Buku2" : "Laut Bercerita",
+#     "Buku3" : "Anak semua bangsa"
+# }    
+
+#key
+#  print(Daftar_buku)["buku2"]
+#print(daftar_buku)
+
+#item
+# for attol in Daftar_buku.items():
+#     print(attol)
+
+#values
+# for attol in Daftar_buku.values():
+    #  print(attol)
+
+
+# Biodata = {
+# "Nama" : "Ananda Daffa Harahap",
+# "NIM" : 2409106050,
+# "KRS" : ["Pemrograman Web", "Struktur Data", "Basis Data"],
+# "Mahasiswa_Aktif" : True,
+# "Social Media" : {
+#     "Instagram" : "daffahrhap"
+# }
+# }
+
+# #print(Biodata["Nama"])
+# print(Biodata["KRS"][1])
+
+# print(Biodata.get("mahasigma"))
+
+
+
+
+
+# list_nama=dict(mahasiswa1="dapupu", mahasiswa2="bambang")
+# print(list_nama)
+
+Film = {
+"Avenger Endgame" : "Action",
+"Sherlock Holmes" : "Mystery",
+"The Conjuring" : "Horror"
+}
+# print(Film)
+# Film["The Conjuring"] = "Horror"
+# Film.update({" minion ": "Thrillertr"})
+
+print(Film)
+Film["Sherlock Holmes"] = "Action"
+Film.update({"The Conjuring" : "Tragedy"})
+
+
---
+++ kelas/pertemuan6/pertemuan-6.py
@@ -0,0 +1,11 @@
+# set
+buah = {"apel", "jeruk", "mangga", "apel"}
+print(buah)
+
+angka_ganjil = {1, 3, 5, 7, 9}
+for angka in angka_ganjil:
+  print(angka)
+
+
+angka_ganjil.add(11)
+print(angka_ganjil)
